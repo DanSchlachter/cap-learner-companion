71d5fe3b63630091626178126fe174f38816f7e8
Daniel Schlachter <schlachter.daniel@gmail.com>
2024-04-15 17:41:31 +0200

    typo

--- learner-companion/app.js
+++ learner-companion/app.js
@@ -1,14 +1,14 @@
 var Mocha = require("mocha");
 var fs = require("fs");
 const { join } = require("path");
-let current_excercise = 1;
+let current_exercise = 1;
 let failed = false;
 
-const test = (excercise) => {
-  copyExcerciseText(excercise);
-  console.log(`Testing excercise ${excercise}`);
+const test = (exercise) => {
+  copyExerciseText(exercise);
+  console.log(`Testing exercise ${exercise}`);
   var mocha = new Mocha({});
-  mocha.addFile(join(__dirname, `/x/${excercise}/test.js`));
+  mocha.addFile(join(__dirname, `/x/${exercise}/test.js`));
   mocha.bail(true);
 
   mocha
@@ -24,15 +24,15 @@ const test = (excercise) => {
     .on("end", function () {
       mocha.unloadFiles();
       mocha.dispose();
-      const x = getNumberOfExcercises(join(__dirname, "/x"));
+      const x = getNumberOfExercises(join(__dirname, "/x"));
       if (failed) {
-        console.log(`Excercise ${excercise} failed!`);
-      } else if (excercise === x) {
+        console.log(`Exercise ${exercise} failed!`);
+      } else if (exercise === x) {
         console.log("All tests passed! Congratulations!");
         copyFile(join(__dirname, "/x/bye.md"));
       } else {
-        current_excercise++;
-        test(current_excercise);
+        current_exercise++;
+        test(current_exercise);
       }
     })
     .on("error", function (err) {
@@ -48,19 +48,19 @@ watch(project, { recursive: true, filter: f => !/README.md/.test(f) }, function 
   //run tests
   console.log("File changed: ", name);
   failed = false;
-  test(current_excercise);
+  test(current_exercise);
 });
 
-const copyExcerciseText = (excercise) => {
-  copyFile(join(__dirname, `/x/${excercise}/task.md`));
+const copyExerciseText = (exercise) => {
+  copyFile(join(__dirname, `/x/${exercise}/task.md`));
 }
 const copyFile = (file) => {
   fs.copyFileSync(join(file), join(project, `README.md`));
 }
-test(current_excercise);
-copyExcerciseText(current_excercise);
+test(current_exercise);
+copyExerciseText(current_exercise);
 
-const getNumberOfExcercises = (source) => getDirectories(source).length;
+const getNumberOfExercises = (source) => getDirectories(source).length;
 
 const getDirectories = (source) =>
   fs
--- learner-companion/x/1/test.js
+++ learner-companion/x/1/test.js
@@ -1,9 +1,12 @@
+process.env.CDS_TEST_SILENT='y'
 const cds = require('@sap/cds/lib')
 const exp = require('constants')
 const { join } = require('path')
 const project = join(__dirname, '../../../incidents-app')
 const { expect } = cds.test(project)
 const schema_cds = join(project, '/db/schema.cds')
+
+
 
 describe('cds.compile.to.edmx tests', ()=>{
   it('should compile the model', async ()=>{
